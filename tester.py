215f10af1024b3cf7e5bdee333c160d4edfa2950
michael chang <michaelc@berkeley.edu>
2017-05-24 21:50:44 -0700

    adding a file to test git hooks

---
+++ tester.py
@@ -0,0 +1,7 @@
+# testing the new commit hooks!!
+import numpy as np
+
+print 'hi mi chaena owiefha weoifhaeow;ifhaweiflhaew oifha ;ilfhawo;eifaw;eoiflha;weoifaw ;ifhaw;oeifhaoiwea;oiwehfaiweh fawef'
+
+for x in range(10):
+    print 'hi'
