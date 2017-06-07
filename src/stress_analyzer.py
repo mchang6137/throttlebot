bc566228c720d8e599aac8c3c745b3cd80ca6610
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2017-06-07 11:20:09 -0700

    Added skeleton file of stress_analyzer

---
+++ src/stress_analyzer.py
@@ -0,0 +1,16 @@
+from remote_execution import *
+
+
+# Returns a dictionary of the services and their respective container_ids
+# VM_IPS is a list of ip addresses, and SERVICES is a list of services
+def get_container_ids(vm_ips, services):
+
+    return
+
+
+# Returns an updated dictionary of the containers to stress
+# OLD_CONTAINERS is the previous dictionary of containers, and RESULTS are the
+#     results of the stress tests
+def get_updated_container_ids(old_containers, results):
+    # Will implement later
+    return
