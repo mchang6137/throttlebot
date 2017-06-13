9ea608c15cc017a791b8d2398d3c028e5289af00
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2017-06-13 13:25:22 -0700

    Modified necessary functions in present_results.py to be compatible with the new data structure holding stress results

--- src/present_results.py
+++ src/present_results.py
@@ -17,22 +17,34 @@ def read_from_file(data_file):
     with open(data_file) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            service = row['service']
+            container_id = row['container_id']
             metric = row['metric']
             stress_level = row['increment']
             resource = row['resource']
             results = row['data_points']
 
+            if service not in cpu:
+                cpu[service] = {}
+                disk[service] = {}
+                network[service] = {}
+
+            if container_id not in cpu:
+                cpu[service][container_id] = {}
+                disk[service][container_id] = {}
+                network[service][container_id] = {}
+
             if metric not in cpu:
-                cpu[metric] = {}
-                disk[metric] = {}
-                network[metric] = {}
+                cpu[service][container_id][metric] = {}
+                disk[service][container_id][metric] = {}
+                network[service][container_id][metric] = {}
 
             if resource == 'cpu':
-                cpu[metric][stress_level] = results
+                cpu[service][container_id][metric][stress_level] = results
             elif resource == 'disk':
-                disk[metric][stress_level] = results
+                disk[service][container_id][metric][stress_level] = results
             elif resource == 'network':
-                network[metric][stress_level] = results
+                network[service][container_id][metric][stress_level] = results
 
     return cpu, disk, network
 
@@ -92,24 +104,24 @@ def append_results_to_file(cpu, disk, network, experiment_type, use_causal_analy
         causal_output = 'singlestress'
     output_file_name = '{}_{}_{}_{}.csv'.format(experiment_type, causal_output, iterations, datetime.datetime.now())
 
-    all_metric_keys = disk[0].keys()
-
     with open(OUTPUT_DIRECTORY + output_file_name, 'wb') as csvfile:
-        fieldnames = ['metric', 'increment', 'resource', 'mean', 'stddev', 'data_points']
+        fieldnames = ['service', 'container_id', 'metric', 'increment', 'resource', 'mean', 'stddev', 'data_points']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        #Iterate through the metrics
-        for metric in all_metric_keys:
-            for increment_key in sorted(disk.iterkeys()):
+        for service, (container, data) in disk:
+            #Iterate through the metrics
+            all_metric_keys = data[0].keys()
+            for metric in all_metric_keys:
+                for increment_key in sorted(data.iterkeys()):
                 #Iterate through different metrics
-                results_cpu = cpu[increment_key][metric]
-                results_disk = disk[increment_key][metric]
-                results_network = network[increment_key][metric]
+                    results_cpu = cpu[service][container][increment_key][metric]
+                    results_disk = disk[service][container][increment_key][metric]
+                    results_network = network[service][container][increment_key][metric]
 
-                writer.writerow({'metric': metric, 'increment': increment_key, 'resource': 'cpu', 'mean': numpy.mean(results_cpu), 'stddev': numpy.std(results_cpu), 'data_points': results_cpu})
-                writer.writerow({'metric': metric, 'increment': increment_key, 'resource': 'disk', 'mean': numpy.mean(results_disk), 'stddev': numpy.std(results_disk), 'data_points': results_disk})
-                writer.writerow({'metric': metric, 'increment': increment_key, 'resource': 'network', 'mean': numpy.mean(results_network), 'stddev': numpy.std(results_network), 'data_points': results_network})
+                    writer.writerow({'service': service, 'container_id': container, 'metric': metric, 'increment': increment_key, 'resource': 'cpu', 'mean': numpy.mean(results_cpu), 'stddev': numpy.std(results_cpu), 'data_points': results_cpu})
+                    writer.writerow({'service': service, 'container_id': container, 'metric': metric, 'increment': increment_key, 'resource': 'disk', 'mean': numpy.mean(results_disk), 'stddev': numpy.std(results_disk), 'data_points': results_disk})
+                    writer.writerow({'service': service, 'container_id': container, 'metric': metric, 'increment': increment_key, 'resource': 'network', 'mean': numpy.mean(results_network), 'stddev': numpy.std(results_network), 'data_points': results_network})
 
     return OUTPUT_DIRECTORY + output_file_name
 
@@ -131,90 +143,92 @@ def plot_results(data_file, experiment_type, iterations, should_save, convertToM
     if not os.path.exists(OUTPUT_DIRECTORY + output_dir_name):
         os.makedirs(OUTPUT_DIRECTORY + output_dir_name)
 
-    # Iterate through the metrics
-    for metric in cpu.iterkeys():
-        box_array_cpu = []
-        box_array_disk = []
-        box_array_network = []
-        axis_labels = []
+    for service,(container,data) in cpu:
+        # Iterate through the metrics
+        for metric in data.iterkeys():
+            box_array_cpu = []
+            box_array_disk = []
+            box_array_network = []
+            axis_labels = []
 
-        baseline_results = numpy.mean(ast.literal_eval(cpu[metric]['0']))
-        print 'baseline results is {}'.format(baseline_results)
+            baseline_results = numpy.mean(ast.literal_eval(data[metric]['0']))
+            print 'baseline results is {}'.format(baseline_results)
 
-        for increment_key in sorted(disk[metric].iterkeys()):
-            # Iterate through different metrics
-            results_cpu = ast.literal_eval(cpu[metric][increment_key])
-            results_disk = ast.literal_eval(disk[metric][increment_key])
-            results_network = ast.literal_eval(network[metric][increment_key])
+            for increment_key in sorted(data[metric].iterkeys()):
+                # Iterate through different metrics
+                results_cpu = ast.literal_eval(cpu[service][container][metric][increment_key])
+                results_disk = ast.literal_eval(disk[service][container][metric][increment_key])
+                results_network = ast.literal_eval(network[service][container][metric][increment_key])
 
-            # results_cpu = [result - baseline_results for result in results_cpu]
-            # results_disk = [result - baseline_results for result in results_disk]
-            # results_network = [result - baseline_results for result in results_network]
+                # results_cpu = [result - baseline_results for result in results_cpu]
+                # results_disk = [result - baseline_results for result in results_disk]
+                # results_network = [result - baseline_results for result in results_network]
 
-            print '==========================================================='
-            print 'THE METRIC IS: {}'.format(metric)
-            print 'For Key: {}'.format(increment_key)
-            print 'CPU stats:'
-            print 'Mean: {}'.format(numpy.mean(results_cpu))
-            print 'Standard Deviation: {}\n'.format(numpy.std(results_cpu))
+                print '==========================================================='
+                print 'FOR CONTAINER {} OF SERVICE {}'.format(container, service)
+                print 'THE METRIC IS: {}'.format(metric)
+                print 'For Key: {}'.format(increment_key)
+                print 'CPU stats:'
+                print 'Mean: {}'.format(numpy.mean(results_cpu))
+                print 'Standard Deviation: {}\n'.format(numpy.std(results_cpu))
 
-            print 'Disk Stats: '
-            print 'Mean: {}'.format(numpy.mean(results_disk))
-            print 'Standard Deviation: {}\n'.format(numpy.std(results_disk))
+                print 'Disk Stats: '
+                print 'Mean: {}'.format(numpy.mean(results_disk))
+                print 'Standard Deviation: {}\n'.format(numpy.std(results_disk))
 
-            print 'Network Stats: '
-            print 'Mean: {}'.format(numpy.mean(results_network))
-            print 'Standard Deviation: {}\n'.format(numpy.std(results_network))
+                print 'Network Stats: '
+                print 'Mean: {}'.format(numpy.mean(results_network))
+                print 'Standard Deviation: {}\n'.format(numpy.std(results_network))
 
-            axis_labels.append(increment_key)
-            try:
-                temp_array_cpu = numpy.array(results_cpu)
-                # no_outliers_cpu = temp_array_cpu[~is_outlier(results_cpu)]
-                box_array_cpu.append(temp_array_cpu)
-            except:
-                print 'CPU FAILED'
+                axis_labels.append(increment_key)
+                try:
+                    temp_array_cpu = numpy.array(results_cpu)
+                    # no_outliers_cpu = temp_array_cpu[~is_outlier(results_cpu)]
+                    box_array_cpu.append(temp_array_cpu)
+                except:
+                    print 'CPU FAILED'
 
-            try:
-                temp_array_disk = numpy.array(results_disk)
-#                no_outliers_disk = temp_array_disk[~is_outlier(results_disk)]
-                box_array_disk.append(temp_array_disk)
-            except:
-                print 'DISK FAILED'
+                try:
+                    temp_array_disk = numpy.array(results_disk)
+    #                no_outliers_disk = temp_array_disk[~is_outlier(results_disk)]
+                    box_array_disk.append(temp_array_disk)
+                except:
+                    print 'DISK FAILED'
 
-            try:
-                temp_array_network = numpy.array(results_network)
-#                no_outliers_network = temp_array_network[~is_outlier(results_network)]
-                box_array_network.append(temp_array_network)
-            except:
-                print 'NETWORK FAILED'
+                try:
+                    temp_array_network = numpy.array(results_network)
+    #                no_outliers_network = temp_array_network[~is_outlier(results_network)]
+                    box_array_network.append(temp_array_network)
+                except:
+                    print 'NETWORK FAILED'
 
-        ymin = 1.05 * min(find_min_point(box_array_cpu), find_min_point(box_array_disk), find_min_point(box_array_network))
-        ymax = 1.2 * max(find_max_point(box_array_cpu), find_max_point(box_array_disk), find_max_point(box_array_network))
+            ymin = 1.05 * min(find_min_point(box_array_cpu), find_min_point(box_array_disk), find_min_point(box_array_network))
+            ymax = 1.2 * max(find_max_point(box_array_cpu), find_max_point(box_array_disk), find_max_point(box_array_network))
 
-        # Plots the boxplots
-        # plot_boxplot(axis_labels, box_array_disk, metric, 'Disk', 222, use_causal_analysis, ymin, ymax)
-        # plot_boxplot(axis_labels, box_array_network, metric, 'Network', 221, use_causal_analysis, ymin, ymax)
-        # plot_boxplot(axis_labels, box_array_cpu, metric, 'CPU', 223, use_causal_analysis, ymin, ymax)
+            # Plots the boxplots
+            # plot_boxplot(axis_labels, box_array_disk, metric, 'Disk', 222, use_causal_analysis, ymin, ymax)
+            # plot_boxplot(axis_labels, box_array_network, metric, 'Network', 221, use_causal_analysis, ymin, ymax)
+            # plot_boxplot(axis_labels, box_array_cpu, metric, 'CPU', 223, use_causal_analysis, ymin, ymax)
 
-        # Plots through the medians
-        plot_interpolation(box_array_disk, metric, 'Disk')
-        plot_interpolation(box_array_network, metric, 'Network')
-        plot_interpolation(box_array_cpu, metric, 'CPU', experiment_type)
+            # Plots through the medians
+            plot_interpolation(box_array_disk, metric, 'Disk')
+            plot_interpolation(box_array_network, metric, 'Network')
+            plot_interpolation(box_array_cpu, metric, 'CPU', experiment_type)
 
-        plot_flat_baseline(box_array_disk, metric)
+            plot_flat_baseline(box_array_disk, metric)
 
-        plt.legend(loc=(0.05, 0.6))
+            plt.legend(loc=(0.05, 0.6))
 
-        graph_file = OUTPUT_DIRECTORY + output_dir_name + '/' + metric + '.png'
-        graph_file = './' + output_dir_name +  metric + '.png'
+            graph_file = OUTPUT_DIRECTORY + output_dir_name + '/' + metric + '.png'
+            graph_file = './' + output_dir_name +  metric + '.png'
 
-        print output_dir_name
-        if should_save == 'save_pdf' and metric == 'latency_99':
-            pp = PdfPages(output_dir_name + '.pdf')
-            pp.savefig()
-            pp.close()
-#        plt.savefig(graph_file)
-        plt.show()
+            print output_dir_name
+            if should_save == 'save_pdf' and metric == 'latency_99':
+                pp = PdfPages(output_dir_name + '.pdf')
+                pp.savefig()
+                pp.close()
+    #        plt.savefig(graph_file)
+            plt.show()
 
     return
 
