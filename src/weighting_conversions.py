10818f878573d4b606ae3aca80f6476ed988d900
michael chang <michaelc@berkeley.edu>
2017-09-11 18:33:33 -0700

    Testing conducted on nginx, modifications to run_experiment

--- src/weighting_conversions.py
+++ src/weighting_conversions.py
@@ -53,6 +53,9 @@ def weighting_to_cpu_cores(weight_change, current_alloc):
             new_cores = current_alloc - 1
         else:
             new_cores = current_alloc + 1
+    if num_cores <= 0:
+        print 'Cannot shrink the number of cores anymore'
+        return 1
     return new_cores
 
 # This probably belongs in a different function
