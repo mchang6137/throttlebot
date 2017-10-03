7c909fb3a63513879984dc9e87263bc7648e6242
Ubuntu <ubuntu@ip-172-31-5-101.us-west-1.compute.internal>
2017-10-03 17:50:48 +0000

    Revert to older method of proportional scaling

--- src/run_throttlebot.py
+++ src/run_throttlebot.py
@@ -88,6 +88,13 @@ def init_cluster_capacities_r(redis_db, machine_type, quilt_overhead):
 Tools that are used for experimental purposes in Throttlebot 
 '''
 
+def finalize_mr_provision(mr, new_alloc):
+    resource_modifier.set_mr_provision(mr, new_alloc)
+    old_alloc = resource_datastore.read_mr_alloc(redis_db, mr)
+    resource_datastore.write_mr_alloc(redis_db, mr, new_alloc)
+    update_machine_consumption(redis_db, new_alloc, old_alloc)
+    current_mr_config = update_mr_config(redis_db, current_mr_config)
+
 # Takes a list of MRs ordered by score and then returns a list of IMRs and nIMRs
 def seperate_mr(mr_list, baseline_performance, optimize_for_lowest, within_x=0.02):
     imr_list = []
@@ -135,19 +142,19 @@ def check_improve_mr_viability(redis_db, mr, improvement_amount):
 # Decrease resource provisions for co-located resources
 # Assures that every the reduction_proposal will allow every instance of the service to balloon
 # It assumes that given a particular IMR, it is not viable to improve that resource.
-# In the NIMR list for NIMRs to be de-allocated. Will return empty list if no viable solution is found
-def check_decrease_colocated_mr_viability(redis_db, mr, nimr_list):
+# In the NIMR list for NIMRs to be de-allocated.
+# Returns a list of NIMRs to reduce and the raw amount to reduce each NIMR, and the amount to incease the IMR bu
+def create_decrease_nimr_schedule(redis_db, imr, nimr_list, stress_weight):
     # Filter out NIMRs that are not the same resource type as mr
     for nimr in nimr_list:
-        if nimr.resource != mr.resource: nimr_list.remove(nimr)
+        if nimr.resource != imr.resource: nimr_list.remove(nimr)
     
-    nimr_service_name_list = [nimr.service_name for nimr in nimr_list]
     reduction_proposal = []
 
     # Ensure that every deployment has at least one service losing a machine
     vm_to_nimr = {}
     vm_to_service = get_vm_to_service(get_actual_vms())
-
+    
     for deployment in mr.instances:
 	vm_ip,container = deployment
         colocated_services = vm_to_service[vm_ip]
@@ -155,25 +162,31 @@ def check_decrease_colocated_mr_viability(redis_db, mr, nimr_list):
 
         vm_to_nimr[vm_ip] = []
         for colocated_service in colocated_services:
-            if colocated_service in nimr_service_name_list:
-                vm_to_nimr[vm_ip].append(colocated_service)
+            for nimr in nimr_list:
+                if colocated_service == nimr.service_name:
+                    vm_to_nimr[vm_ip].append(nimr)
 
-    # Brute Force Implementation to Start
-    for nimr in nimr_service_name_list:
-        for vm_ip in vm_to_nimr.keys():
-            if nimr in vm_to_nimr[vm_ip]:
-                reduction_proposal.append(nimr)
-                del vm_to_nimr[vm_ip]
-                
-    if len(vm_to_nimr) == 0:
-        return reduction_proposal
-    else:
-        return []
+    min_mr_removal = float('inf')
+    new_mr_alloc = {}
+    
+    for vm_ip in vm_to_nimr:
+        if len(vm_to_nimr[vm_ip]) == 0:
+            return []
+        total_removal_amount = 0
+        for nimr in vm_ip:
+            nimr_alloc = resource_datastore.read_mr_alloc(nimr, mr_resource)
+            new_alloc = convert_percent_to_raw(nimr, nimr_alloc, stress_weight)
+            alloc_diff = nimr_alloc - new_alloc
+            new_mr_alloc[nimr] = alloc_diff
+            total_removal_amount += alloc_diff
+        min_mr_removal = total_removal_amount if total_removal_amount < min_mr_removal
+
+    return new_mr_alloc, min_mr_removal
 
 # Determine if mr still has an interval to decrease it to
-def mr_at_minimum(mr, instance_type, proposed_weight_change):
+def mr_at_minimum(mr, proposed_weight_change):
     current_alloc = mr.read_mr_alloc(redis_db)
-    new_alloc = convert_percent_to_raw(mr, current_alloc, instance_type, proposed_weight_change)
+    new_alloc = convert_percent_to_raw(mr, current_alloc, proposed_weight_change)
     if new_alloc == -1:
         return True
     else:
@@ -280,6 +293,7 @@ def run(system_config, workload_config, filter_config, default_mr_config):
         mr_to_stress = apply_filtering_policy(redis_db,
                                               mr_working_set,
                                               experiment_count,
+                                              system_config,
                                               workload_config,
                                               filter_config)
         
@@ -291,7 +305,7 @@ def run(system_config, workload_config, filter_config, default_mr_config):
             current_mr_allocation = resource_datastore.read_mr_alloc(redis_db, mr)
             print 'Current MR allocation is {}'.format(current_mr_allocation)
             for stress_weight in stress_weights:
-                new_alloc = convert_percent_to_raw(mr, current_mr_allocation, stress_weight, machine_type)
+                new_alloc = convert_percent_to_raw(mr, current_mr_allocation, stress_weight)
                 resource_modifier.set_mr_provision(mr, new_alloc)
                 experiment_results = measure_runtime(workload_config, experiment_trials)
 
@@ -300,7 +314,7 @@ def run(system_config, workload_config, filter_config, default_mr_config):
                 tbot_datastore.write_redis_ranking(redis_db, experiment_count, preferred_performance_metric, mean_result, mr, stress_weight)
 
                 # Remove the effect of the resource stressing
-                new_alloc = convert_percent_to_raw(mr, current_mr_allocation, 0, machine_type)
+                new_alloc = convert_percent_to_raw(mr, current_mr_allocation, 0)
                 resource_modifier.set_mr_provision(mr, new_alloc)
                 increment_to_performance[stress_weight] = experiment_results
 
@@ -321,46 +335,36 @@ def run(system_config, workload_config, filter_config, default_mr_config):
         mimr = None
         action_taken = 0
         
-        for mr in imr_list:
-            improvement_percent = improve_mr_by(redis_db, mr, max_stress_weight)
-            current_mr_allocation = resource_datastore.read_mr_alloc(redis_db, mr)
-            new_alloc = convert_percent_to_raw(mr, current_mr_allocation, improvement_percent, machine_type)
-            improvement_amount = new_alloc - current_mr_allocation
-            action_taken = improvement_amount
+        for imr in imr_list:
+            imr_improvement_percent = improve_mr_by(redis_db, imr, max_stress_weight)
+            current_imr_alloc = resource_datastore.read_mr_alloc(redis_db, imr)
+            new_imr_alloc = convert_percent_to_raw(imr, current_mr_alloc, imr_improvement_percent)
+            imr_improvement_proposal = new_imr_alloc = current_imr_alloc
 
-            nimr_proposal = []
-            if check_improve_mr_viability(redis_db, mr, improvement_amount) is False:
-                # Remove NIMRs that are already at a minimum
-                for nimr in nimr_list:
-                    if mr_at_minimum(nimr, machine_type, max_stress_weight):
-                        nimr_list.remove(nimr)
+            nimr_diff_proposal = {}
+            if check_improve_mr_viability(redis_db, mr, imr_improvement_proposal) is False:
                 # Calculate a plan to reduce the resource provisioning of NIMRs
-                nimr_proposal = check_decrease_colocated_mr_viability(redis_db, mr, nimr_list)
-
-                if len(nimr_proposal) == 0:
+                nimr_diff_proposal,imr_improvement_proposal = create_decrease_nimr_schedule(redis_db, imr, nimr_list, max_stress_weight)
+                if len(nimr_diff_proposal) == 0 or imr_improvement_proposal == 0:
                     continue
 
             # Decrease the amount of resources provisioned to the NIMR
-            for nimr in nimr_proposal:
-                old_alloc = read_mr_alloc(redis_db, nimr)
-                new_alloc = convert_percent_to_raw(nimr, current_alloc, machine_type, max_stress_weight)
-                resource_datastore.write_mr_alloc(redis_db, nimr, new_alloc)
-                update_machine_consumption(redis_db, new_alloc, old_alloc)
+            for nimr in nimr_diff_proposal:
+                new_nimr_alloc = resource_datstore.read_mr_alloc(redis_db, nimr) + nimr_diff_proposal[nimr]
+                finalize_resource_provision(nimr, new_nimr_alloc)
                 current_mr_config = update_mr_config(redis_db, current_mr_config)
-                
 
             # Improving the resource should always be viable at this step
-            if check_improve_mr_viability(redis_db, mr, improvement_amount):
-                resource_modifier.set_mr_provision(mr, new_alloc)
-                print 'Improvement Calculated: MR {} increase from {} to {}'.format(mr.to_string(), current_mr_allocation, new_alloc)
-                old_alloc = resource_datastore.read_mr_alloc(redis_db, mr)
-                resource_datastore.write_mr_alloc(redis_db, mr, new_alloc)
-                update_machine_consumption(redis_db, mr, new_alloc, old_alloc)
+            if check_improve_mr_viability(redis_db, imr, imr_improvement_proposal):
+                new_imr_alloc = imr_improvement_proposal + current_imr_alloc
+                finalize_resource_provision(imr, new_imr_alloc)
+                print 'Improvement Calculated: MR {} increase from {} to {}'.format(mr.to_string(), current_imr_alloc, new_imr_alloc)
                 current_mr_config = update_mr_config(redis_db, current_mr_config)
-                mimr = mr
+                mimr = imr
                 break
             else:
                 print 'Improvement Calculated: MR {} failed to improve from {} to {}'.format(mr.to_string(), current_mr_allocation, new_alloc)
+                print 'This case should never occur. Exiting...'
                 
         if mimr is None:
             print 'No viable improvement found'
--- src/weighting_conversions.py
+++ src/weighting_conversions.py
@@ -1,102 +1,92 @@
 '''
-
-Resource provisioning is assigned in increments of 10. 
-Assigns a new resource provision and then checks to see that the new resource provision is valid.
-
+Converts various weightings between 0 and 100 to an actual amount to stress particular resource to
 ALL UNITS IN BITS
-
 All functions accept:
 1.) Current allocation of resource
 2.) Percentage to change resource. 
 - A positive number means to increase the resource provisioning by a certain amount. 
 - A negative numbers means to decrease the resource provisioning by a certain amount
-
-Returns the newly calculated provisioning, and -1 if the proposed weight change creates an invalid 
-
+Returns the new bandwdith
 '''
 from modify_resources import *
 from remote_execution import *
 
 # Converts a change in resource provisioning to raw change
 # Example: 20% -> 24 Gbps
-def convert_percent_to_raw(mr, current_alloc, instance_type, weight_change=0):
+def convert_percent_to_raw(mr, current_mr_allocation, weight_change=0):
     if mr.resource == 'CPU-CORE':
-        return weighting_to_cpu_cores(weight_change, current_alloc, instance_type)
+        return weighting_to_cpu_cores(weight_change, current_mr_allocation)
     elif mr.resource == 'CPU-QUOTA':
-        return weighting_to_cpu_quota(weight_change, current_alloc, instance_type)
+        return weighting_to_cpu_quota(weight_change, current_mr_allocation)
     elif mr.resource == 'DISK':
-        return  weighting_to_blkio(weight_change, current_alloc, instance_type)
+        return  weighting_to_blkio(weight_change, current_mr_allocation)
     elif mr.resource == 'NET':
-        return weighting_to_net_bandwidth(weight_change, current_alloc, instance_type)
+        return weighting_to_net_bandwidth(weight_change, current_mr_allocation)
     elif mr.resource == 'MEMORY':
-        return weighting_to_memory(weight_change, current_alloc, instance_type, mr.instances)
+        return weighting_to_memory(weight_change, current_mr_allocation, mr.instances)
     else:
         print 'INVALID resource'
         exit()
-
+        
 # Change the networking capacity
 # Current Capacity in bits/p
-def weighting_to_net_bandwidth(weight_change, current_alloc, instance_type):
-    total_allocation = get_instance_specs(instance_type)['NET']
-    new_bandwidth = current_alloc + ((weight_change / 100.0) * total_allocation)
-    if new_bandwidth > 0:
-        return int(new_bandwidth)
-    else:
-        return -1
+def weighting_to_net_bandwidth(weight_change, current_alloc):
+    new_bandwidth = current_alloc + ((weight_change / 100.0) * current_alloc)
+    assert new_bandwidth > 0
+    return int(new_bandwidth)
 
 # Change the weighting on the blkio
 # Conducted for the disk stressing
-def weighting_to_blkio(weight_change, current_alloc, instance_type):
+def weighting_to_blkio(weight_change, current_alloc):
     #+10 because blkio only accepts values between 10 and 1000
-        #Lower weighting must have lower bound on the blkio weight allocation
-    total_allocation = 1000
-    new_blkio = current_alloc + int((weight_change / 100.0) * total_allocation + 10)
-    if new_blkio > 0:
-        return int(new_blkio)
-    else:
-        return -1
+    #Lower weighting must have lower bound on the blkio weight allocation
+    new_blkio = current_alloc + int((weight_change / 100.0) * current_alloc + 10)
+    assert new_blkio > 0
+    return int(new_blkio)
 
 # Change the weighting of the CPU Quota
 # TODO: Extend this to type of stressing to multiple cores
 # Assumes a constant period
-def weighting_to_cpu_quota(weight_change, current_alloc, instance_type):
-    total_alloc = 100 * get_instance_specs(instance_type)['CPU-CORE']
+def weighting_to_cpu_quota(weight_change, current_alloc):
     # We divide by 100 because CPU quota allocation is given as percentage
-    new_quota = current_alloc + (total_alloc * weight_change/100.0)
-    if new_quota > 0:
-        return int(new_quota)
-    else:
-        return -1
+    new_quota = current_alloc + current_alloc * weight_change/100.0
+    assert new_quota > 0
+    return new_quota
 
 # Alternative method of changing the CPU stresing
 # Reduces the number of cores
 # This is a special case, unlike the other types of stressing
-def weighting_to_cpu_cores(weight_change, current_alloc, instance_type):
-    total_alloc = 100 * get_instance_specs(instance_type)['CPU-CORE']
+def weighting_to_cpu_cores(weight_change, current_alloc):
     assert current_alloc > 0
-    
-    new_cores = round(current_alloc + (weight_change / 100.0) * total_alloc)
+
+    new_cores = round(current_alloc + (weight_change / 100.0) * current_alloc)
     if new_cores == current_alloc:
         if weight_change < 0:
             new_cores = current_alloc - 1
         else:
             new_cores = current_alloc + 1
-    if num_cores <= 0:
-        print 'Cannot shrink the number of cores anymore'
-        return 1
+            if num_cores <= 0:
+                print 'Cannot shrink the number of cores anymore'
+            return 1
     return new_cores
 
-def weighting_to_memory(weight_change, current_alloc, instance_type, instance):
-    total_alloc = 100 * get_instance_specs(instance_type)['MEMORY']
-    new_memory = current_alloc + int(total_alloc * weight_change/100.0)
+def weighting_to_memory(weight_change, current_alloc, instance):
+    new_memory = current_alloc + int(current_alloc * weight_change/100.0)
     min_memory = get_min_memory(instance[0])
-    if new_memory > min_memory:
-        return int(new_memory)
-    else:
-        return -1
-    
+    if new_memory < min_memory:
+        return min_memory
+    return new_memory
 
 # Units in MB
 def get_min_memory(instance):
     return 200  # Hardcoded for now
+
+# This probably belongs in a different function
+# Leaving here for convenience
+def get_num_cores(ssh_client):
+    num_cores_cmd = 'nproc --all'
+    _, stdout, _ = ssh_client.exec_command(num_cores_cmd)
+    return int(stdout.read())
+
+
 
