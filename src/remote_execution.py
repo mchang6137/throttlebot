2b1113d5039b47d09742a0cd7669abc3073e0a2e
Emerson Hsieh <emersonhsieh@berkeley.edu>
2018-06-19 21:13:47 -0700

    Added return_error flag

--- src/remote_execution.py
+++ src/remote_execution.py
@@ -6,8 +6,8 @@ def get_client(ip):
     client.connect(ip, username="quilt", password="")
     return client
 
-def ssh_exec(ssh_client, cmd, contains_container_id=False):
-    _, _, stderr = ssh_client.exec_command(cmd)
+def ssh_exec(ssh_client, cmd, contains_container_id=False, return_error=False):
+    _,stdout,stderr = ssh_client.exec_command(cmd)
     err = stderr.read()
     if err:
         print "Command contains container_id: {}".format(contains_container_id)
@@ -15,6 +15,9 @@ def ssh_exec(ssh_client, cmd, contains_container_id=False):
         # Bug: only error upon container change error.
         if contains_container_id:
             raise SystemError('Container ID error for mr')
+    
+    if return_error:
+        return _,stdout,stderr
 
 def close_client(ssh_client):
     ssh_client.close()
