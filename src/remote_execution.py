4ba51a39e39f50b68e94fc45a5f6f016d980b275
Emerson Hsieh <emersonhsieh@berkeley.edu>
2018-06-19 21:08:32 -0700

    Added contains_container_id flag for ssh_exec in remote_execution to seperate container_id errors from all other errors.

--- src/remote_execution.py
+++ src/remote_execution.py
@@ -6,13 +6,15 @@ def get_client(ip):
     client.connect(ip, username="quilt", password="")
     return client
 
-def ssh_exec(ssh_client, cmd):
+def ssh_exec(ssh_client, cmd, contains_container_id=False):
     _, _, stderr = ssh_client.exec_command(cmd)
     err = stderr.read()
     if err:
+        print "Command contains container_id: {}".format(contains_container_id)
         print ("Error execing {}: {}".format(cmd, err))
         # Bug: only error upon container change error.
-        raise SystemError('Container does not exist anymore')
+        if contains_container_id:
+            raise SystemError('Container ID error for mr')
 
 def close_client(ssh_client):
     ssh_client.close()
