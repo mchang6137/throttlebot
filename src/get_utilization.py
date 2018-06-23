0dbcb45fbedbd5015ce649ec93382ae14052aa59
Emerson Hsieh <emersonhsieh@berkeley.edu>
2018-06-22 21:51:26 -0700

    Removed spaces for memory utilization

--- src/get_utilization.py
+++ src/get_utilization.py
@@ -7,8 +7,13 @@ from remote_execution import *
 # Gets the current memory utilization in bytes
 def get_current_memory_utilization(ssh_client, container_id):
     get_memory_cmd = 'cat /sys/fs/cgroup/memory/docker/{}*/memory.usage_in_bytes'.format(container_id)
-    _,memory_utilization,_ = ssh_exec(ssh_client, get_memory_cmd, modifies_container=True, return_error=True)
+    _,memory_utilization_obj,stderr = ssh_exec(ssh_client, get_memory_cmd, modifies_container=True, return_error=True)
+
+    memory_utilization_string = memory_utilization_obj.read()
+    memory_utilization_float = float("".join(memory_utilization_string.split()))
+
     print "Recovering the memory utilization"
-    return float(memory_utilization.read())
-    
+    print memory_utilization_float
+
+    return memory_utilization_float
     
