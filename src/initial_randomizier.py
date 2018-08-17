2e383a6566b9d0d029e61f53cdca566e3bfbc9ad
Will Wang <hwang97@berkeley.edu>
2018-08-17 03:44:58 +0000

    randomizier now does a fake run to determine viability

--- src/initial_randomizier.py
+++ src/initial_randomizier.py
@@ -1,9 +1,11 @@
-import csv, random
+import csv, random, logging
+from mr import MR
+from run_throttlebot import fake_run, parse_resource_config_file, parse_config_file
 
 tiers = {
     1: ["osalpekar/spark-image-compress..."],
-    2: ["elasticsearch:2.4", "library/postgres", "tsaianson/node-apt-app", "haproxy:1.7", "hantaowang/logstash-postgres"],
-    3: ["kibana:4", "mysql:5"],
+    2: ["elasticsearch:2.4", "library/postgres:9.4", "tsaianson/node-apt-app", "haproxy:1.7", "hantaowang/logstash-postgres"],
+    3: ["kibana:4", "mysql:5.6.32"],
 }
 
 resources = ["CPU-QUOTA", "DISK", "NET"]
@@ -15,7 +17,7 @@ def identify_tier_resources(mr_allocation):
         assert mr.resource in resources
         for tier, lst in tiers.items():
             if mr.service_name in lst:
-                tier_mr = MR(tier, mr.resource)
+                tier_mr = MR(tier, mr.resource, [])
                 if tier_mr in tier_resources:
                     assert tier_resources[tier_mr] == value
                 else:
@@ -25,8 +27,8 @@ def identify_tier_resources(mr_allocation):
     for resource in resources:
         total = 0
         for i in range(1, 4):
-            total += tier_resources[MR(i, resource)]
-        tier_resources[MR(0, resource)] = total
+            total += tier_resources[MR(i, resource, [])]
+        tier_resources[MR(0, resource, [])] = total
 
     return tier_resources
 
@@ -58,40 +60,24 @@ def scramble_tiers(tier_resources, times):
             if r != not_changed:
                 split = random.choice(splits)
                 for i in range(1, 4):
-                    tier_resources[MR(i, r)] = split[i - 1] * tier_resources[MR(0, r)]
-                key.append((r, split))
+                    tier_resources[MR(i, r, [])] = split[i - 1] * tier_resources[MR(0, r, [])]
+                key.append((r, split, []))
         if key not in done:
             t += 1
             yield tier_resources.copy()
 
-def tier_to_configuration(tier_resources, config):
+def tier_to_configuration(tier_resources):
+    config = {}
     for i in range(1, 4):
         for r in resources:
-            value = tier_resources[MR(i, r)]
+            value = tier_resources[MR(i, r, [])]
             for service in tiers[i]:
-                config[MR(service, r)] = value
+                config[MR(service, r, [])] = value
     return config
 
 # ------------------------------------------------------------------------------------
 # ------------------- Functions adapted or copied from Throttlebot -------------------
 # ------------------------------------------------------------------------------------
-class MR:
-    def __init__(self, service_name, resource):
-        self.service_name = service_name
-        self.resource = resource
-
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        else:
-            return False
-
-    def __hash__(self):
-        return hash((self.service_name, self.resource))
-
-    def __repr__(self):
-        return str((self.service_name, self.resource))
-
 def print_csv_configuration(final_configuration, output_csv='tuned_config.csv'):
     with open(output_csv, 'w') as csvfile:
         fieldnames = ['SERVICE', 'RESOURCE', 'AMOUNT', 'REPR']
@@ -106,50 +92,51 @@ def print_csv_configuration(final_configuration, output_csv='tuned_config.csv'):
             writer.writerow(result_dict)
 
 
-def parse_resource_config_file(resource_config_csv):
-
-    mr_allocation = {}
-
-    # Manual Configuration Possible
-    # Parse a CSV
-    # Format of resource allocation: SERVICE,RESOURCE,TYPE,REPR
-    with open(resource_config_csv, 'rb') as resource_config:
-        reader = csv.DictReader(resource_config)
-
-        for row in reader:
-            service_name = row['SERVICE']
-            resource = row['RESOURCE']
-            amount = float(row['AMOUNT'])
-            amount_repr = row['REPR']
-
-            assert amount_repr == 'RAW'
-
-            mr = MR(service_name, resource)
-            mr_allocation[mr] = amount
-
-    return mr_allocation
-
 # ------------------------------------------------------------------------------------
 # --------------------------------------- Main ---------------------------------------
 # ------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    mr_allocation = parse_resource_config_file("tuned_config.csv")
+
+    # Setup Logging
+    logging.basicConfig(level=logging.INFO)
+    logging.info('Started Logging')
+    
+    sys_config, workload_config, filter_config = parse_config_file("test_config.cfg")
+    mr_allocation = parse_resource_config_file(None, sys_config)
+    
+    baseline = fake_run("test_config.cfg", mr_allocation)
+    baseline = sum(baseline) / len(baseline)
+
+    seperator = "-----------------------------------------"
+    print seperator
+    print "Baseline:", baseline
+    print seperator
+
     tier_resources = identify_tier_resources(mr_allocation)
     def write_config(title, tier_resources, f):
         f.write("{}\n".format(title))
         for i in range(1, 4):
             f.write("    Tier {}: {}\n".format(i, tiers[i]))
             for r in resources:
-                f.write("        {}: {}\n".format(r, tier_resources[MR(i, r)]))
+                f.write("        {}: {}\n".format(r, tier_resources[MR(i, r, [])]))
 
     with open("./configs/report.txt", "w") as f:  
         write_config("Original Baseline", tier_resources, f)
         trial = 0
 
-        for s in scramble_tiers(tier_resources, 10):
+        for s in scramble_tiers(tier_resources, 20):
+            config = tier_to_configuration(s)
+            print_csv_configuration(config, "./configs/config-{}.csv".format(trial))
+            config = parse_resource_config_file("./configs/config-{}.csv".format(trial), sys_config)
+            t = fake_run("test_config.cfg", config)
+            t = sum(t) / len(t)
+            print seperator
+            print "Baseline:", baseline, "Trial", t
+            print seperator
+            if t > baseline:
+                continue
+
             write_config("Config {}".format(trial), tier_resources, f)
             trial += 1
-            config = tier_to_configuration(s, mr_allocation)
-            print_csv_configuration(config, "./configs/config-{}.csv".format(trial))
 
 
--- src/run_throttlebot.py
+++ src/run_throttlebot.py
@@ -101,7 +101,6 @@ def init_cluster_capacities_r(redis_db, machine_type, quilt_overhead):
     min_alloc = get_instance_min_specs()
 
     quilt_usage = {}
-
     # Leave some resources available for Quilt containers to run (OVS, etc.)
      # This is dictated by quilt overheads
     for resource in resource_alloc:
@@ -582,7 +581,7 @@ workload_config: Parameters about the workload in a dict
 default_mr_config: Filtered MRs that should be stress along with their default allocation
 '''
 
-def run(sys_config, workload_config, filter_config, default_mr_config, last_completed_iter=0):
+def run(sys_config, workload_config, filter_config, default_mr_config, last_completed_iter=0, fake=False):
     redis_host = sys_config['redis_host']
     baseline_trials = sys_config['baseline_trials']
     experiment_trials = sys_config['trials']
@@ -656,6 +655,11 @@ def run(sys_config, workload_config, filter_config, default_mr_config, last_comp
 
     current_performance[preferred_performance_metric] = remove_outlier(current_performance[preferred_performance_metric])
     baseline_performance = current_performance[preferred_performance_metric]
+    
+    # If fake, then return only baseline
+    if fake:
+    	return baseline_performance
+
     current_time_stop = datetime.datetime.now()
     time_delta = current_time_stop - time_start
 
@@ -1372,6 +1376,26 @@ def update_mr_id(redis_db, mr_to_change):
     else:
         mr_to_change.instances = new_instance_locations
         logging.info("Container ID replaced successfully")
+
+# Remove this afterwards
+def fake_run(config_file, mr_allocation):
+
+    sys_config, workload_config, filter_config = parse_config_file(config_file)
+
+    mr_allocation = filter_mr(mr_allocation,
+                              sys_config['stress_these_resources'],
+                              sys_config['stress_these_services'],
+                              sys_config['stress_these_machines'])
+
+    all_vm_ip = get_actual_vms()
+    workload_config['request_generator'] = [get_master()]
+    services = get_service_placements(all_vm_ip)
+    workload_config['frontend'] = [services['haproxy:1.7'][0][0]]
+    print "Retrieving frontend:", workload_config['frontend']
+    print "Retrieving request_generator:", workload_config['request_generator']
+    
+    r = run(sys_config, workload_config, filter_config, mr_allocation, 0, fake=True)
+    return r
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
