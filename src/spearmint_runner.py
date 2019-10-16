cab7fd70ff2178c3d0ed501ff28a07eeca2b86b5
Rahul Balakrishnan <rahulb@berkeley.edu>
2019-10-15 18:24:14 -0700

    Changing from time_to_beat to polling for best run so far

--- src/spearmint_runner.py
+++ src/spearmint_runner.py
@@ -8,43 +8,40 @@ import shlex
 import glob
 
 
-def run(iterations, time_to_beat):
+def run(iterations, time_to_beat, duration, polling_frequency):
 
-    date_time = datetime.now()
-    subprocess.Popen(shlex.split("mkdir /Users/rahulbalakrishnan/Desktop/data/experiment_run-{}"
-                                 .format(date_time.strftime("%m-%d-%Y-%H-%M-%S"))))
+    # date_time = datetime.now()
+    # subprocess.Popen(shlex.split("mkdir /Users/rahulbalakrishnan/Desktop/data/experiment_run-{}"
+    #                              .format(date_time.strftime("%m-%d-%Y-%H-%M-%S"))))
 
-    results = []
-    trials = []
-    best_runs = []
+    cumulative_results = []
 
     for _ in range(iterations):
 
         print("Starting iteration {}".format(_))
 
-        start_time = time.time()
+        # start_time = time.time()
 
         queue = mp.Queue()
 
+        cmd = "python2.7 ./spearmint/spearmint/main.py --driver=local --method=GPEIOptChooser " + \
+              "--method-args=noiseless=1 --data-file=test.csv /Users/rahulbalakrishnan/Desktop/" + \
+              "throttlebot/src/spearmint/bayOptSearch/bayOpt.pb"
 
-        process_expr = mp.Process(target = run_experiment, args = (queue,))
-        process_poll = mp.Process(target=poll_for_best_result, args = (queue, time_to_beat, process_expr))
+        p = subprocess.Popen(shlex.split(cmd), shell=False)
 
-        process_expr.start()
+
+
+        process_poll = mp.Process(target=poll_for_best_result, args = (queue, time_to_beat, p, duration,
+                                                                       polling_frequency))
+
         process_poll.start()
 
         process_poll.join()
-        process_expr.join()
 
-        print("Saving results for iteration {}".format(_))
+        cumulative_results.append(queue.get())
 
-        results.append(queue.get() - start_time)
-
-        trials.append(len(glob.glob("/Users/rahulbalakrishnan/Desktop/throttlebot/src/spearmint/bayOptSearch/output/*")))
-
-        best_runs.append(queue.get())
-
-        print("Saving results for iteration {} to disk".format(_))
+        print("Done with iteration {}".format(_))
 
 
         subprocess.check_output(["bash ./spearmint/spearmint/save.sh"], shell=True)
@@ -55,48 +52,39 @@ def run(iterations, time_to_beat):
     date_time = datetime.now()
     with open("/Users/rahulbalakrishnan/Desktop/data/threshold/data_{}"
                       .format(date_time.strftime("%m-%d-%Y-%H-%M-%S")), "w") as f:
-            dct_to_write = {"time_to_beat": time_to_beat, "data": results, "iterations": iterations,
-                            "trials_until_goal": trials, "best_runs": best_runs}
-            f.write(json.dumps(dct_to_write))
+
+            f.write(json.dumps({"results": cumulative_results, "polling_frequency": polling_frequency}))
+
+def poll_for_best_result(queue, time_to_beat, process_to_terminate, duration, polling_frequency):
+
+    starting_time = time.time()
+    time_to_compare = starting_time
+    while time.time() - starting_time < duration:
 
 
-
-
-
-def run_experiment(queue):
-    cmd = "python2.7 ./spearmint/spearmint/main.py --driver=local --method=GPEIOptChooser " + \
-                             "--method-args=noiseless=1 --data-file=test.csv /Users/rahulbalakrishnan/Desktop/" + \
-                             "throttlebot/src/spearmint/bayOptSearch/bayOpt.pb"
-
-    p = subprocess.Popen(shlex.split(cmd), shell=False)
-
-    queue.put(p)
-
-
-def poll_for_best_result(queue, time_to_beat, process_to_terminate):
-
-    while True:
         try:
-            cmd = "grep \'Best result\' /Users/rahulbalakrishnan/Desktop/throttlebot/src/spearmint/" \
-                  "bayOptSearch/best_job_and_result.txt"
 
-            output = str(subprocess.check_output([cmd], shell=True).decode("utf-8"))
+            current_time = time.time()
+            if current_time - time_to_compare >= polling_frequency:
+                time_to_compare = current_time
+                cmd = "grep \'Best result\' /Users/rahulbalakrishnan/Desktop/throttlebot/src/spearmint/" \
+                      "bayOptSearch/best_job_and_result.txt"
 
-            if float(output[13:-1]) < time_to_beat:
-                queue.put(time.time())
-                queue.put(float(output[13:-1]))
-                break
+                output = str(subprocess.check_output([cmd], shell=True).decode("utf-8"))
+                trial = len(glob.glob("/Users/rahulbalakrishnan/Desktop/throttlebot/src/spearmint/bayOptSearch/output/*"))
+                queue.put([float(output[13:-1]), trial])
+
             else:
-                time.sleep(5)
+                time.sleep(max(polling_frequency, 2))
+
         except:
-            time.sleep(5)
+            time.sleep(max(polling_frequency, 2))
 
 
 
-    p = queue.get()
-    p.kill()
-    process_to_terminate.terminate()
+
+    process_to_terminate.kill()
 
 
 
-run(20, 1500)
+run(iterations=20, time_to_beat=4000, duration=5*60, polling_frequency=30)
